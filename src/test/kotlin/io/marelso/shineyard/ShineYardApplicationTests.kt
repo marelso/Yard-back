77695f39605a9665f330e322830aca9a9e2babd4
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-03 07:21:25 -0300

    Initial create

---
+++ src/test/kotlin/io/marelso/shineyard/ShineYardApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.marelso.shineyard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShineYardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
