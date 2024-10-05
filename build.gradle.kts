9d1ff5cca49660f62c5e7f153552ee594d57a37a
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-05 07:20:57 -0300

    Including firebase sdk dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+	implementation("com.google.firebase:firebase-admin:9.3.0")
+
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 
 	compileOnly("org.projectlombok:lombok")
