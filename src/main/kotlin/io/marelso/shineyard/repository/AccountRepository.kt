684bbd658ef7417930967ebb23284487962098f7
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-05 05:23:12 -0300

    Including new repository layer to handle account database operations

---
+++ src/main/kotlin/io/marelso/shineyard/repository/AccountRepository.kt
@@ -0,0 +1,9 @@
+package io.marelso.shineyard.repository
+
+import io.marelso.shineyard.domain.Account
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface AccountRepository: MongoRepository<Account, String> {
+}
