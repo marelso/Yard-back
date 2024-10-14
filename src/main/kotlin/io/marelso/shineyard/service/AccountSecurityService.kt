77da636b091d7e57dfa2285ab166dbd661b46e34
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-14 19:29:44 -0300

    Refactoring account service to use account instead of userDetail

--- src/main/kotlin/io/marelso/shineyard/service/AccountSecurityService.kt
+++ src/main/kotlin/io/marelso/shineyard/service/AccountSecurityService.kt
@@ -2,24 +2,13 @@ package io.marelso.shineyard.service
 
 import io.marelso.shineyard.domain.Account
 import io.marelso.shineyard.repository.AccountRepository
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 
-typealias SecurityUser = Account
-
 @Service
 class AccountSecurityService(private val accountRepository: AccountRepository): UserDetailsService {
-    override fun loadUserByUsername(username: String): UserDetails {
-        return accountRepository.findByEmail(username)?.mapToUserDetails() ?: throw UsernameNotFoundException(username)
-    }
-
-    private fun SecurityUser.mapToUserDetails(): UserDetails {
-        return User.builder()
-            .username(email)
-            .password(password)
-            .build()
+    override fun loadUserByUsername(username: String): Account {
+        return accountRepository.findByEmail(username) ?: throw UsernameNotFoundException(username)
     }
 }
