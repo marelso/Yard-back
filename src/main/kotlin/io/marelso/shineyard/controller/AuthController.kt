b02d066534a842918fbf3499578beb897148c4e0
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-14 19:26:14 -0300

    Renaming file to make more sense

--- src/main/kotlin/io/marelso/shineyard/controller/AuthController.kt
+++ src/main/kotlin/io/marelso/shineyard/controller/AuthController.kt
@@ -1,16 +1,19 @@
 package io.marelso.shineyard.controller
 
-import io.marelso.shineyard.domain.AuthenticationRequestDto
+import io.marelso.shineyard.domain.AccountDto
 import io.marelso.shineyard.service.AuthenticationService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/v1/auth")
 class AuthController(
     private val authenticationService: AuthenticationService
 ) {
     @PostMapping
-    fun auth(@RequestBody request: AuthenticationRequestDto) = ResponseEntity.ok<String>(authenticationService.authenticate(request))
+    fun auth(
+        @RequestParam("email") email: String,
+        @RequestParam("password") password: String
+    ) = ResponseEntity.ok<AccountDto>(authenticationService.authenticate(email, password))
 }
