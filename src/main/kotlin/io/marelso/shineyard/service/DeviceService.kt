752b096fb9280402fe1c923da92a187c61a1774b
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-14 19:56:21 -0300

    removing line break

--- src/main/kotlin/io/marelso/shineyard/service/DeviceService.kt
+++ src/main/kotlin/io/marelso/shineyard/service/DeviceService.kt
@@ -5,9 +5,7 @@ import io.marelso.shineyard.repository.RemoteRepository
 import org.springframework.stereotype.Service
 
 @Service
-class DeviceService(
-    private val database: RemoteRepository
-) {
+class DeviceService(private val database: RemoteRepository) {
     fun getDeviceById(id: String): Device? = database.getDeviceById(id)
 
     fun findAllByIds(accountDevices: List<String>): List<Device> = mutableListOf<Device>().apply {
