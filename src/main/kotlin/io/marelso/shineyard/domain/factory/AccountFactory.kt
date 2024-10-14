50a877a5e1a02c7c870fd8314b42acd1b4613e19
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-14 19:33:43 -0300

    Including new factory to handle account creation

---
+++ src/main/kotlin/io/marelso/shineyard/domain/factory/AccountFactory.kt
@@ -0,0 +1,21 @@
+package io.marelso.shineyard.domain.factory
+
+import io.marelso.shineyard.domain.Account
+import io.marelso.shineyard.domain.Device
+import io.marelso.shineyard.domain.dto.AccountDto
+import org.springframework.stereotype.Component
+
+@Component
+class AccountFactory {
+    fun from(account: Account, devices: List<Device>, jwt: String ): AccountDto {
+        return AccountDto(
+            id = account.id,
+            firstName = account.firstName,
+            lastName = account.lastName,
+            pictureUrl = account.pictureUrl,
+            email = account.email,
+            jwt = jwt,
+            accountDevices = devices,
+        )
+    }
+}
