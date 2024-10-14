9de4171bcdf4888f45cc361da383b3e6c68177f1
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-14 19:28:25 -0300

    Updating account signature

--- src/main/kotlin/io/marelso/shineyard/domain/Account.kt
+++ src/main/kotlin/io/marelso/shineyard/domain/Account.kt
@@ -1,21 +1,27 @@
 package io.marelso.shineyard.domain
 
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
 data class Account(
     val id: String? = null,
     val firstName: String,
     val lastName: String,
     val pictureUrl: String,
     val email: String,
-    val password: String,
+    @JvmField val password: String,
     val accountDevices: List<String>,
     val accountTokens: List<String>
-)
+) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return mutableSetOf()
+    }
 
-data class AccountDto(
-    val id: String? = null,
-    val firstName: String,
-    val lastName: String,
-    val pictureUrl: String,
-    val email: String,
-    val accountDevices: List<Device>
-)
+    override fun getPassword(): String {
+        return this.password
+    }
+
+    override fun getUsername(): String {
+        return this.email
+    }
+}
