01bbe32f40be8511af1ac9a0d3820dd905989e06
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-19 19:43:49 -0300

    Including bean if missing

--- src/main/kotlin/io/marelso/shineyard/config/AppConfig.kt
+++ src/main/kotlin/io/marelso/shineyard/config/AppConfig.kt
@@ -1,4 +1,16 @@
 package io.marelso.shineyard.config
 
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
 class AppConfig {
+    @Bean
+    @ConditionalOnMissingBean(AmazonS3::class)
+    fun s3(): AmazonS3 = AmazonS3ClientBuilder.standard()
+        .withPathStyleAccessEnabled(true)
+        .build()
 }
