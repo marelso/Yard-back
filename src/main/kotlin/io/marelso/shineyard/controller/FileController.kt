66eee4d27858bf378c505b323a4b3256f41bee5a
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-10-19 19:44:53 -0300

    Including new controller to handle file upload

--- src/main/kotlin/io/marelso/shineyard/controller/FileController.kt
+++ src/main/kotlin/io/marelso/shineyard/controller/FileController.kt
@@ -1,4 +1,18 @@
 package io.marelso.shineyard.controller
 
-class FileController {
+import io.marelso.shineyard.service.S3Service
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+
+@RestController
+@RequestMapping("/api/v1/auth/img")
+class FileController(private val s3Service: S3Service) {
+    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    @Operation(summary = "Upload an image to the yard' image repository.")
+    fun uploadFile(@RequestParam("file") file: MultipartFile): String =  s3Service.uploadFile(file)
 }
